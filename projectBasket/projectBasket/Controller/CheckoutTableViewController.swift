eab2238727ab40423d369aef3a8d02102c363450
Tiago Mendes <nomadmovement@gmail.com>
2017-06-06 00:12:55 +0100

    initial commit

---
+++ projectBasket/projectBasket/Controller/CheckoutTableViewController.swift
@@ -0,0 +1,9 @@
+//
+//  CheckoutTableViewController.swift
+//  projectBasket
+//
+//  Created by Tiago Mendes on 05/06/2017.
+//  Copyright © 2017 mendes. All rights reserved.
+//
+
+import Foundation
---
+++ projectBasket/projectBasket/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  projectBasket
+//
+//  Created by Tiago Mendes on 05/06/2017.
+//  Copyright © 2017 mendes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
